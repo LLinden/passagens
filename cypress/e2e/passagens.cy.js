d5394b5464ebfcbba2f5d47eefc496b1a372c6f8
Lucas Di Franco Linden <lucas.linden@cwi.com.br>
2023-06-29 16:21:31 -0300

    first commit

--- cypress/e2e/passagens.cy.js
+++ cypress/e2e/passagens.cy.js
@@ -14,24 +14,12 @@ describe("Passagens", () => {
         let captcha = url.includes("captcha");
         if (captcha == true) {
           cy.url().should("equal", 0);
-          // tenta burlar captcha
-          //cy.get(el.botaoCaptcha).trigger("mousedown", {
-          //button: 10,
-          //});
         }
       });
     });
-    // cy.get(el.menorPreco)
-    //   .invoke("text")
-    //   .then((text) => {
-    //     cy.wrap(text).as("menor");
-    //     cy.log("@menor");
-    //     cy.writeFile("../resultados.txt", menor, { flag: "a+" });
-    //   });
   });
 
   it("deve capturar menor preço", () => {
-    //cy.writeFile("../fixtures/resultados.txt", "@menor", { flag: "a+" });
     cy.get(el.menorPreco).then(($menor) => {
       let txt = $menor.text();
       cy.writeFile("../resultados.txt", txt, { flag: "a+" });
